2f159adf9e8ef709456bce0c65529620292fdc1a
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-08-31 21:18:47 -0500

    modified:   pom.xml 	modified:   src/main/java/com/campussurvey/campussurvey/WebConfig.java 	modified:   target/classes/com/campussurvey/campussurvey/WebConfig.class

--- src/main/java/com/campussurvey/campussurvey/WebConfig.java
+++ src/main/java/com/campussurvey/campussurvey/WebConfig.java
@@ -1,17 +1,54 @@
 package com.campussurvey.campussurvey;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("uncledave")
+            .password("123")
+            .roles("ADMIN")  
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .and()
+            .httpBasic()
+            .and()
+            .csrf().disable(); // Deshabilitar protección CSRF;
+        
+        return http.build();
     }
 }
