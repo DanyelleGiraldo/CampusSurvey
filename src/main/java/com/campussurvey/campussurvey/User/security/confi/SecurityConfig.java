e3b13b541fb5b3390cc05f0ccd650f82e18029d0
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-09-03 14:41:31 -0500

    modified:   src/main/java/com/campussurvey/campussurvey/User/security/confi/SecurityConfig.java 	modified:   target/classes/com/campussurvey/campussurvey/User/security/confi/SecurityConfig.class

--- src/main/java/com/campussurvey/campussurvey/User/security/confi/SecurityConfig.java
+++ src/main/java/com/campussurvey/campussurvey/User/security/confi/SecurityConfig.java
@@ -4,7 +4,6 @@ package com.campussurvey.campussurvey.User.security.confi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
