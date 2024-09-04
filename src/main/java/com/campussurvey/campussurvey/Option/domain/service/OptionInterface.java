cd11b1e7e298a77d602ca3a571c7efaa4cd9446d
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-09-04 16:44:40 -0500

    new file:   src/main/java/com/campussurvey/campussurvey/Option/application/OptionServiceImpl.java 	renamed:    src/main/java/com/campussurvey/campussurvey/Option/domain/service/OptionService.java -> src/main/java/com/campussurvey/campussurvey/Option/domain/service/OptionInterface.java 	new file:   src/main/java/com/campussurvey/campussurvey/Option/infrastructure/OptionController.java 	new file:   target/classes/com/campussurvey/campussurvey/Option/application/OptionServiceImpl.class 	new file:   target/classes/com/campussurvey/campussurvey/Option/domain/service/OptionInterface.class 	deleted:    target/classes/com/campussurvey/campussurvey/Option/domain/service/OptionService.class 	new file:   target/classes/com/campussurvey/campussurvey/Option/infrastructure/OptionController.class

--- src/main/java/com/campussurvey/campussurvey/Option/domain/service/OptionInterface.java
+++ src/main/java/com/campussurvey/campussurvey/Option/domain/service/OptionInterface.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.campussurvey.campussurvey.Option.domain.entities.Option;
 
-public interface OptionService {
+public interface OptionInterface {
     void save(Option option);
     void delete(Option option);
     void update(Long id, Option option);
